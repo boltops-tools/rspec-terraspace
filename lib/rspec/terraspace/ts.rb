b0c6df310f0ba3cbd16ce8117667520fba5c5799
Tung Nguyen <tongueroo@gmail.com>
2020-05-20 17:11:04 +0000

    output method

--- lib/rspec/terraspace/ts.rb
+++ lib/rspec/terraspace/ts.rb
@@ -1,12 +1,16 @@
+require "json"
+
 module RSpec::Terraspace
   class Ts
+    extend Memoist
+
     CLI = ::Terraspace::CLI
 
     def build_test_harness(options={})
       puts "build_test_harness"
       project = Project.new(options)
-      ts_root = project.create
-      ENV['TS_ROOT'] = ts_root # switch root to the generated test harness
+      @ts_root = project.create
+      ENV['TS_ROOT'] = @ts_root # switch root to the generated test harness
     end
 
     def up(args)
@@ -20,6 +24,21 @@ module RSpec::Terraspace
     def run(command)
       args = command.split(' ')
       CLI.start(args)
+    end
+
+    def save_output(mod)
+      run("output #{mod} --json --save-to #{saved_output_path}")
+    end
+    memoize :save_output
+
+    def output(mod, name)
+      save_output(mod)
+      data = JSON.load(IO.read(saved_output_path))
+      data.dig(name, "value")
+    end
+
+    def saved_output_path
+      "#{@ts_root}/output.json"
     end
   end
 end
