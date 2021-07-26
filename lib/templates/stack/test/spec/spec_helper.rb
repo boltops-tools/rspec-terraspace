2c846e102792928a5cc1219b2deb95d11dffff84
Tung Nguyen <tongueroo@gmail.com>
2021-07-26 01:13:09 +0000

    update project level template

--- lib/templates/stack/test/spec/spec_helper.rb
+++ lib/templates/stack/test/spec/spec_helper.rb
@@ -13,6 +13,12 @@ module Helper
 end
 
 RSpec.configure do |c|
-  c.include Helper
-  c.include RSpec::Terraspace::Helpers
+  c.before(:all) do
+    Dir.glob("config/helpers/**/*.rb").each do |path|
+      require "./#{path}"
+      name = path.sub(%r{config/helpers/},'').sub('.rb','').camelize
+      mod = "Terraspace::Project::#{name}"
+      c.include mod.constantize
+    end
+  end
 end
